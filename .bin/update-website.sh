dd79f15035d4c658b92bc03698682c95eda9d722
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-02-12 15:50:00 -0700

    Website update script

---
+++ .bin/update-website.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CWD="$(pwd)"
+REPO_DIR="$(basename "$CWD")"
+REPO_PARENT_DIR="$(basename "$(dirname "$CWD")")"
+
+if [[ "$REPO_DIR" == "chateaumaxmin.info" ]] && [[ "$REPO_PARENT_DIR" == "_repos" ]] && [[ -d ../../chateaumaxmin.info ]]; then
+	find . -mindepth 1 \
+	       -maxdepth 1 \
+	       -not -iname '.bin' \
+	       -not -iname '.git' \
+	       -not -iname '.gitignore' \
+	       -not -iname 'README.md' \
+	       -exec rm -rf "{}" \;
+
+	cp -rf ../../chateaumaxmin.info/* .
+else
+	echo "Unexpected execution directory"
+	exit 1
+fi
